8f2189a603db20cb2e4ad7f76b50e8fc62824772
Moritz Freyburger <moritz@freyburger.io>
2023-04-10 20:40:14 +0200

    Update version, provide multiarch

--- tetrs.rb
+++ tetrs.rb
@@ -1,9 +1,16 @@
 class Tetrs < Formula
   desc "Command line Tetris written in Rust"
   homepage "https://github.com/FreyMo/tetrs"
-  url "https://github.com/FreyMo/tetrs/releases/download/v0.0.2/tetrs-aarch64-apple-darwin.tar.gz"
-  sha256 "25969c7045d943113f26553bb988684e64cab7dc0c5aa0cdb945f245c12cc072"
-  version "0.0.2"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/FreyMo/tetrs/releases/download/v0.0.3/tetrs-aarch64-apple-darwin.tar.gz"
+    sha256 "d8bd10dfa362efd56e0199e7894cbbc4124644a5e84637c386cda866e9643da1"
+  else
+    url "https://github.com/FreyMo/tetrs/releases/download/v0.0.3/tetrs-x86_64-apple-darwin.tar.gz"
+    sha256 "fc88af38fa3ecec4f0b91623c8cdfde20400f97a466eccf40af395561230fcf2"
+  end
+
+  version "0.0.3"
 
   def install
     bin.install "tetrs"
